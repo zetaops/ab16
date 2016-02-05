c1854b8d5ad387d4b6ff898b6fe90b0d7417d152
Evren Esat Özkan <evrenesat@zetaops.io>
2016-02-05 14:58:53 +0200

    ~

--- ab16/models.py
+++ ab16/models.py
@@ -1,12 +1,6 @@
 # -*-  coding: utf-8 -*-
 """
 """
-
-# Copyright (C) 2015 ZetaOps Inc.
-#
-# This file is licensed under the GNU General Public License v3
-# (GPLv3).  See LICENSE.txt for details.
-
 from pyoko import Model, LinkProxy, ListNode
 from pyoko import field
 from pyoko.exceptions import ObjectDoesNotExist
--- ab16/settings.py
+++ ab16/settings.py
@@ -1,11 +1,6 @@
 # -*-  coding: utf-8 -*-
 """
 """
-
-# Copyright (C) 2015 ZetaOps Inc.
-#
-# This file is licensed under the GNU General Public License v3
-# (GPLv3).  See LICENSE.txt for details.
 from pyoko.settings import *
 
 DEFAULT_BUCKET_TYPE = 'ab16'
